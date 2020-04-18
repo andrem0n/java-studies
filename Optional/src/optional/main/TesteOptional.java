9b963db6ec77e9b0146d452e8f30ad06d61a0998
André Bolzan Egert <andre.bolzan@ntconsult.com.br>
2020-04-18 20:34:00 -0300

    optional code

--- Optional/src/optional/main/TesteOptional.java
+++ Optional/src/optional/main/TesteOptional.java
@@ -1,14 +1,17 @@
 package optional.main;
 
+import java.util.Objects;
 import java.util.Optional;
 import optional.model.Seguradora;
 
 public class TesteOptional {
 
   public static void main(String[] args) {
-    Seguradora seguradora = new Seguradora("peeei");
+    Seguradora seguradora = new Seguradora("Planalto");
     System.out.println(obterNomeSeguradora(seguradora));
     System.out.println(obterNomeSeguradoraOptional(seguradora));
+    checarNomeSeguradora(seguradora);
+    checarNomeSeguradoraOptional(seguradora);
   }
 
   public static String obterNomeSeguradoraOptional(Seguradora seguradora) {
@@ -20,5 +23,16 @@ public class TesteOptional {
       return seguradora.getNome();
     }
     return null;
+  }
+
+  public static void checarNomeSeguradora(Seguradora seguradora) {
+    if (Objects.nonNull(seguradora) && seguradora.getNome().equals("Planalto")) {
+      System.out.println("É uma seguradora Planalto");
+    }
+  }
+
+  public static void checarNomeSeguradoraOptional(Seguradora seguradora){
+    Optional.ofNullable(seguradora).filter(s -> s.getNome().equals("Planalto"))
+        .ifPresent(s -> System.out.println("É uma seguradora Planalto"));
   }
 }
