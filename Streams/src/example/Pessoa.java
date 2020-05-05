8b41d8e851f6f00bcbcb94129e8e3968f94f1cac
André Bolzan Egert <andre.bolzan@ntconsult.com.br>
2020-05-05 01:37:30 -0300

    streams examples

--- Streams/src/example/Pessoa.java
+++ Streams/src/example/Pessoa.java
@@ -2,6 +2,7 @@ package example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Pessoa {
 
@@ -62,16 +63,38 @@ public class Pessoa {
         '}';
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Pessoa pessoa = (Pessoa) o;
+    return idade == pessoa.idade &&
+        Objects.equals(id, pessoa.id) &&
+        Objects.equals(nome, pessoa.nome) &&
+        Objects.equals(nacionalidade, pessoa.nacionalidade);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, nome, nacionalidade, idade);
+  }
+
   public List<Pessoa> populaPessoas() {
     Pessoa pessoa1 = new Pessoa("p1", "Matheus Henrique", "Brasil", 18);
     Pessoa pessoa2 = new Pessoa("p2", "Hernandez Roja", "Mexico", 21);
     Pessoa pessoa3 = new Pessoa("p3", "Mario Fernandes", "Canada", 22);
     Pessoa pessoa4 = new Pessoa("p4", "Clodoaldo José", "Brasil", 22);
+    Pessoa pessoa5 = new Pessoa("p4", "Clodoaldo José", "Brasil", 22);
     List<Pessoa> list = new ArrayList<Pessoa>();
     list.add(pessoa1);
     list.add(pessoa2);
     list.add(pessoa3);
     list.add(pessoa4);
+    list.add(pessoa5);
     return list;
   }
 }
--- Streams/src/example/main/Main.java
+++ Streams/src/example/main/Main.java
@@ -1,7 +1,9 @@
 package example.main;
 
 import example.Pessoa;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -11,9 +13,60 @@ public class Main {
 
     List<Pessoa> pessoas = new Pessoa().populaPessoas();
 
+    //uso do filter + map
     Stream<String> stream = pessoas.stream()
-        .filter(pessoa -> pessoa.getNacionalidade().equals("Brasil"))
+        .filter(pessoa -> pessoa.getNacionalidade().equalsIgnoreCase("Brasil"))
         .map(Pessoa::getNome);
-    stream.forEach(System.out::println);
+    //stream.forEach(System.out::println);
+
+    //uso do map com conversor para int
+    IntStream streamMap = pessoas.stream()
+        .filter(pessoa -> pessoa.getNacionalidade().equalsIgnoreCase("Brasil"))
+        .mapToInt(Pessoa::getIdade);
+    //streamMap.forEach(System.out::println);
+
+    //uso do sorted
+    Stream<String> sortedStream = pessoas.stream()
+        .filter(pessoa -> pessoa.getNacionalidade().equalsIgnoreCase("Brasil"))
+        .sorted(Comparator.comparing(Pessoa::getNome))
+        .map(Pessoa::getNome);
+    //sortedStream.forEach(System.out::println);
+
+    //uso distinct
+    Stream<Pessoa> distinctStream = pessoas.stream().distinct();
+    //distinctStream.forEach(System.out::println);
+
+    //uso limit
+    Stream<Pessoa> limitStream = pessoas.stream().limit(3);
+    //limitStream.forEach(System.out::println);
+
+    //usando forEach para imprimir nomes
+    //pessoas.stream().forEach(pessoa -> System.out.println(pessoa.getNome()));
+
+    //uso do average
+    Double media = pessoas.stream()
+        .filter(pessoa -> pessoa.getNacionalidade().equalsIgnoreCase("Brasil"))
+        .mapToInt(Pessoa::getIdade)
+        .average()
+        .getAsDouble();
+    //System.out.println("Média entre idades: " + media);
+
+    //uso do collect(pode transformar para list, set e map)
+    List<String> pessoasComM = pessoas.stream()
+        .filter(pessoa -> pessoa.getNome().startsWith("M"))
+        .map(Pessoa::getNome)
+        .collect(Collectors.toList());
+    //pessoasComM.forEach(System.out::println);
+
+    //uso do count
+    long quantpessoas = pessoas.stream()
+        .filter(pessoa -> pessoa.getNome().startsWith("N"))
+        .count();
+    //System.out.println(quantpessoas);
+
+    //uso do allmatch
+    boolean todosMexicanos = pessoas.stream()
+        .allMatch(pessoa -> pessoa.getNacionalidade().equalsIgnoreCase("Mexico"));
+    //System.out.printf("Todos são mexicanos? " + todosMexicanos);
   }
 }
